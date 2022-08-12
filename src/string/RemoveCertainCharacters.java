5c22db6e73c952b8f98729897c6e1e99ccaf4af1
zackary <zhew5mail@gmail.com>
2022-08-12 13:22:19 -0700

    new coding problems.

--- src/string/RemoveCertainCharacters.java
+++ src/string/RemoveCertainCharacters.java
@@ -1,5 +1,57 @@
 package string;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Remove given characters in input string, the relative order of other characters should be remained. Return the new string after deletion.
+ *
+ * Assumption:
+ * The given input is not null
+ * The characters to be removed is given by another string, it is guaranteed to be not null
+ *
+ * Examples:
+ * input: "abcd", t "ab", delete all instances of 'a' and 'b' results in "cd"
+ */
 public class RemoveCertainCharacters {
+
+    public String removeCertainCharacters(String input, String t) {
+        //String is immutable in Java, so we have to convert the input String into a char array
+        char[] array = input.toCharArray(); //time complexity is O(n), space complexity is O(n)
+
+        //we need to maintain a set to includes all the characters to be removed
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < t. length(); i++) {
+            set.add(t.charAt(i));
+        }
+
+        //to remove characters and keep the same original sequence, we can use two pointers that move in the same direction
+        //the characters between [0, i) are the characters we are going to keep
+        //the characters between [i, j) are the characters we don't care
+        //the character at j is the character we are current checking
+        //the characters between (j, array.length - 1] are the characters to be traversed
+        //for example
+        // a  b  c  d
+        // i
+        // j
+        // we use the j to traverse the character array
+        // if array[j] belongs to the set we are going to remove, -> j++
+        // if array[j] doesn't belong to the set, means we need to keep the character at array[j], so we copy array[j] to array[i], then do i++, j++
+        // eventually we just return the characters between[0, i) as the new string
+
+        int i = 0;
+        int j = 0;
+        while (j < array.length) {  //time complexity is O(n)
+            if (!set.contains(array[j])) {
+                array[i] = array[j];
+                i++;
+            }
+            j++;
+        }
+
+        return new String(array, 0, i); // //time complexity is O(n)
+    }
+
+    //total time complexity is O(n), space complexity is O(n)
 
 }
---
+++ src/string/RemoveSpaces.java
@@ -0,0 +1,90 @@
+package string;
+
+/**
+ * Given a string, remove all leading/trailing/duplicated empty spaces.
+ *
+ * Assumptions:
+ * 1. the give string is not null.
+ *
+ * examples:
+ *  " a" -> "a"
+ *  " I  love MTV " -> "I love MTV"
+ */
+public class RemoveSpaces {
+
+    public String removeSpaces(String input) {
+        //The high level idea is to traverse the input string, when we meet a character
+        //if it is not a space, we keep it
+        //if it is space
+        //1. if the previous character is not a space, we need to keep it
+        //2. if the previous character is a space, we need to ignore it
+        //corner cases: if the string starts with space, then the space needs to be ignored
+
+        //we can use two pointers to solve the problem
+        //[0, i) represents the characters we keep
+        //[i, j) represents the characters we don't care
+        //j is the character we are visiting
+        //(j, n- 1] represents the characters to be visited.
+
+        char[] array = input.toCharArray();
+        int i = 0;
+        int j = 0;
+        while (j < array.length) {
+//            if (array[j] == ' ') {
+//                //i == 0 means we haven't kept any characters, so cannot copy the current space to i position, otherwise the result string will start with space
+//                //array[i - 1] == ' ' means the previous characters we kept is a space, we should ignore this space
+//                if (i == 0 || array[i - 1] == ' ') {
+//                    j++;
+//                } else {
+//                    array[i] = array[j];
+//                    i++;
+//                    j++;
+//                }
+//            } else {
+//                array[i] = array[j];
+//                i++;
+//                j++;
+//            }
+
+            if (array[j] == ' ' && (i == 0 || array[i - 1] == ' ')) {
+                j++;
+                continue;
+            }
+            array[i] = array[j];
+            i++;
+            j++;
+        }
+
+        //after all the operations, we cannot guarantee that the last characters is not a space
+        if (i != 0 && array[i - 1] == ' ') {
+            i--;
+        }
+        return new String(array, 0, i);
+    }
+    //time complexity is O(n)
+
+    /**
+     * Another method using StringBuilder
+     */
+    public String removeSpacesUsingStringBuilder(String input) {
+        // Write your solution here
+        if (input == null || input.length() == 0) {
+            return input;
+        }
+        StringBuilder string = new StringBuilder(input);
+        int i = 0;
+        for (int j = 0; j < input.length(); j++){
+            if (string.charAt(j) == ' ' && (i == 0 || string.charAt(i - 1) == ' ')) {
+                continue;
+            }
+            string.setCharAt(i, string.charAt(j));
+            i++;
+        }
+        if  (i != 0 && string.charAt(i - 1) == ' ') {
+            i--;
+        }
+        //setLength() is good method for StringBuilder to set length.
+        string.setLength(i);
+        return string.toString();
+    }
+}
