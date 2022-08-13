7daab56677fe276adc6f2fbbd9d03e27722001a1
zackary <zhew5mail@gmail.com>
2022-08-12 23:55:42 -0700

    longestConsecutiveOnes

--- src/string/LongestSubstringWithoutRepeatingCharacters.java
+++ src/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -47,4 +47,69 @@ public class LongestSubstringWithoutRepeatingCharacters {
         maxLength = Math.max(maxLength, queue.size());
         return maxLength;
     }
+
+
+    /**
+     * Given an array of integers that contains only 0s and 1s and a positive integer k, you can flip at most k 0s to 1s, return the longest subarray that contains only integer 1 after flipping.
+     * <p>
+     * Assumptions:
+     * 1. length of the given array is between[1, 20000]
+     * 2. The given array only contains 1s and 0s.
+     * 3. 0 <= k <= length of given array.
+     * <p>
+     * Examples:
+     * 1. input: array = [1, 1, 0, 0, 1, 1, 1, 0, 0, 0], k = 2, output: 7
+     * Explanation: flip 0s at index 2 and 3, then the array becomes [1,1,1,1,1,1,1,0,0,0],
+     * so that the length of the longest subarray that contains only integer 1 is 7.
+     * <p>
+     * 2. input: array =[1, 1, 0, 0, 1, 1, 1, 0, 0, 0], k = 0, output: 3
+     * Explanation: k is 0, so you cannot flip any 0 to 1, then the length of the longest subarray that contains only integer 1 is 3.
+     *
+     * @param nums input array
+     * @param k    the maximum flipping times
+     * @return
+     */
+    public int longestSubarrayContainsOnlyOnes(int[] nums, int k) {
+
+        /*
+           maintain a sliding window: the sliding window contains only 1s after flipping
+           two pointers:
+           slow: the left border of the sliding window
+           fast: the current index under processing, fast - 1 is the right border of the sliding window
+
+           we need another variable count to record the number of flipping 0s in the sliding window
+           if character is 1, fast++
+           if character is 0, but count < k, fast++, count++;
+           if character is 0, but the count == k, then we have to move the slow pointer until it reaches the first index on the right side of the first flipping zeros
+
+         */
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int slow = 0;
+        int fast = 0;
+        int flippingZerosCount = 0;
+        int maxLength = 0;
+        while (fast < nums.length) {
+            if (nums[fast] == 1) {
+                fast++;
+            } else if (nums[fast] == 0 && flippingZerosCount < k) {
+                fast++;
+                flippingZerosCount++;
+            } else {
+                maxLength = Math.max(maxLength, fast - slow);
+                while (nums[slow] != 0) {
+                    slow++;
+                }
+                slow++;
+                flippingZerosCount--;
+            }
+        }
+        //we need update maxLength once we reach to the end of the array
+        maxLength = Math.max(maxLength, fast - slow);
+        return maxLength;
+    }
+
 }
